1f6e95260ea93f9cf1078a9baf9c099a4c86d1fb
Kevin <chengrq@shenghesun.com>
2018-09-01 11:31:23 +0800

    kevin 提交master 测试controller

---
+++ mercury-branch/src/main/java/com/kevin/mercury/BranchAndVersionApplication.java
@@ -0,0 +1,11 @@
+package com.kevin.mercury;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchAndVersionApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BranchAndVersionApplication.class, args);
+	}
+}
